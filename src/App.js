729ff21d5d35cfebad3d4f1811f543ae40732e07
Rohandabakra <dabakara21rohan@gmail.com>
2023-02-10 19:13:48 +0530

    Created the App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo, productDataThree } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Products heading='Reviews' data={productDataThree} />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Products/data.js
@@ -0,0 +1,125 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+import person1 from '../../images/person1.jpg';
+import person2 from '../../images/person2.jpg';
+import person3 from '../../images/person3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Italian Pizza',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$28.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Mexican Paradise',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$20.99',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '$12.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+];
+
+
+export const productDataThree = [
+  {
+    img: person1,
+    alt: 'Zain Chacha',
+    name: 'Zain Taln',
+    desc:
+      'Best pizza i have ever had in my life. Italian Pizza had the best toppings.',
+    button: '⭐⭐⭐⭐'
+  },
+  {
+    img: person2,
+    alt: 'Jazz Milan',
+    name: 'Jazz Milan',
+    desc:
+      'Double fudge brownie was very delicious. Once you have it you cannot stop eating.',
+    button: '⭐⭐⭐⭐⭐'
+  },
+  {
+    img: person3,
+    alt: 'Tom Jack',
+    name: 'Tom Jack',
+    desc:
+      'Pizza had a very good flavour, Sauses was very delicious and mouth watering.',
+    button: '⭐⭐⭐⭐⭐'
+  },
+
+];
